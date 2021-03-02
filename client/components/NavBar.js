3cd4df5bd468733166ead76c3818d59054ac6659
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-03-01 23:38:40 -0800

    ADD: HamburgerMenu and name to NavBar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './NavBar.css';
+import "./NavBar.css";
+import HamburgerMenu from "./HamburgerMenu";
 
 const NavBar = () => {
-	return (
-		<nav className="navbar-container flex jcfe">
-			<div className="navbar-list flex jcsb">
-				<Link to="/projects">Projects</Link>
-				<Link to="/about">About</Link>
-				<Link to="/contact">Contact</Link>
-			</div>
-		</nav>
-	);
+  const { innerWidth: width, innerHeight: height } = window;
+
+  return (
+    <nav className="navbar-container flex jcsb">
+      <div className="navbar-list name flex jcfs">
+        <Link to="/">Gerald Lou Berzuela</Link>
+      </div>
+
+      <div className="navbar-list flex jcsb">
+        {width <= 768 ? (
+          <HamburgerMenu />
+        ) : (
+          <>
+            <Link to="/projects">Projects</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
