4ef6d5253b2dae5f972c993d1b4a04323d551f44
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-03-01 17:44:23 -0800

    ADD: HomePage content

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react';
+
+import './HomePage.css';
 
 const HomePage = () => {
-  return <div>Hello from Home Page</div>;
+	return (
+		<div
+			className="homepage-container"
+			aria-label="Image by: Nubelson Fernandes on Unsplash"
+		>
+			<div className="homepage-text-container">
+				<p className="homepage-intro">Hi, I'm Gerald.</p>
+				<p className="homepage-text">I am a..</p>
+				<p className="homepage-text">Student..</p>
+				<p className="homepage-text">Software engineer..</p>
+				<p className="homepage-text">Fitness enthusiast.</p>
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
