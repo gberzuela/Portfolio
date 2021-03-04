c10bea4251fa728b884f4fcd2efdc24472b303f9
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-03-04 13:45:06 -0800

    FIX: NavBar underline animation

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./NavBar.css";
 import HamburgerMenu from "./HamburgerMenu";
 
 const NavBar = () => {
-  const { innerWidth: width, innerHeight: height } = window;
+  const [width, setWidth] = useState(window.innerWidth);
+
+  window.addEventListener("resize", () => {
+    setWidth(window.innerWidth);
+  });
 
   return (
     <nav className="navbar-container flex jcsb">
-      <div className="navbar-list name flex jcfs">
+      <div className="navbar-nav name flex jcfs">
         <Link to="/">Gerald Lou Berzuela</Link>
       </div>
 
-      <div className="navbar-list flex jcsb">
-        {width <= 768 ? (
+      {width <= 768 ? (
+        <div className="navbar-nav hamburger flex">
           <HamburgerMenu />
-        ) : (
-          <>
-            <Link to="/projects">Projects</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-          </>
-        )}
-      </div>
+        </div>
+      ) : (
+        <div className="navbar-nav list flex jcsb">
+          <Link to="/projects">Projects</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      )}
     </nav>
   );
 };
