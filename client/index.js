c9e458d122912c829845e539ac2e6eecb4c791a3
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-03-14 09:22:41 -0700

    ADD: MUI Theme provider

--- client/index.js
+++ client/index.js
@@ -2,15 +2,37 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
 import { NavBar } from "./components";
 import Routes from "./routes";
 
+const theme = createMuiTheme({
+  typography: {
+    allVariants: {
+      color: "white",
+    },
+  },
+  palette: {
+    divider: "#5c5c5c",
+    paper: "#424242",
+    blue: "#0c9ef3",
+  },
+  breakpoints: {
+    values: {
+      md: 767,
+    },
+  },
+});
+
 const App = () => {
   return (
-    <Router>
-      <NavBar />
-      <Routes />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <NavBar />
+        <Routes />
+      </Router>
+    </ThemeProvider>
   );
 };
 
