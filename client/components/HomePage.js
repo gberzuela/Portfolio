b059d88b214cb0cdd60c9e028c4dd46aa0074c32
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-03-16 13:10:44 -0700

    UPDATE: trying to get smooth scroll behavior to work

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React from 'react';
 
-import HomePageTitle from "./HomePageTitle";
-import About from "./About";
-import CustomTimeline from "./CustomTimeline";
-import { Divider, makeStyles } from "@material-ui/core";
+import HomePageTitle from './HomePageTitle';
+import About from './About';
+import CustomTimeline from './CustomTimeline';
+import { Divider, makeStyles } from '@material-ui/core';
 
 const styles = makeStyles({
-  divider: {
-    margin: "100px 20% 100px 20%",
-  },
+	divider: {
+		margin: '0 20% 0 20%',
+	},
 });
 
 const HomePage = () => {
-  const classes = styles();
+	const classes = styles();
 
-  return (
-    <div>
-      <HomePageTitle />
-      <Divider className={classes.divider} />
-      <About />
-      <Divider className={classes.divider} />
-      <CustomTimeline />
-    </div>
-  );
+	return (
+		<>
+			<HomePageTitle />
+			<Divider className={classes.divider} />
+			<About />
+			<Divider className={classes.divider} />
+			<CustomTimeline />
+		</>
+	);
 };
 
 export default HomePage;
--- client/components/MyDrawer.js
+++ client/components/MyDrawer.js
@@ -1,85 +1,74 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
 
 import {
-  Divider,
-  Drawer,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  makeStyles,
-  Toolbar,
-} from "@material-ui/core";
-import HomeIcon from "@material-ui/icons/Home";
-import ComputerIcon from "@material-ui/icons/Computer";
-import HistoryIcon from "@material-ui/icons/History";
-import ContactMailIcon from "@material-ui/icons/ContactMail";
+	Divider,
+	Drawer,
+	List,
+	ListItem,
+	ListItemIcon,
+	ListItemText,
+	makeStyles,
+	Toolbar,
+	Link,
+} from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
+import ComputerIcon from '@material-ui/icons/Computer';
+import HistoryIcon from '@material-ui/icons/History';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
 
 const list = [
-  { "About Me": <HomeIcon /> },
-  { Projects: <ComputerIcon /> },
-  { Background: <HistoryIcon /> },
-  { Contact: <ContactMailIcon /> },
+	{ 'About Me': <HomeIcon /> },
+	{ Projects: <ComputerIcon /> },
+	{ Background: <HistoryIcon /> },
+	{ Contact: <ContactMailIcon /> },
 ];
 
 const styles = makeStyles((theme) => ({
-  divider: {
-    background: theme.palette.divider,
-  },
-  drawerPaper: {
-    width: "200px",
-    backgroundColor: theme.palette.paper,
-  },
-  drawerContent: {
-    color: "white",
-  },
+	divider: {
+		background: theme.palette.divider,
+	},
+	drawerPaper: {
+		width: '200px',
+		backgroundColor: theme.palette.paper,
+	},
+	drawerContent: {
+		color: 'white',
+	},
 }));
 
 const MyDrawer = ({ open, setOpen }) => {
-  const classes = styles();
+	const classes = styles();
 
-  return (
-    <Drawer
-      anchor="right"
-      open={open}
-      onClose={() => setOpen(false)}
-      classes={{ paper: classes.drawerPaper }}
-    >
-      <Toolbar />
-      <Divider className={classes.divider} />
-      <List>
-        {list.map((item) => {
-          const text = Object.keys(item)[0];
-          const icon = item[text];
-          return (
-            // <Link to={`/${text.toLowerCase()}`} key={text}>
-            //   <ListItem button>
-            //     <ListItemIcon className={classes.drawerContent}>
-            //       {icon}
-            //     </ListItemIcon>
-            //     <ListItemText
-            //       primary={text}
-            //       className={classes.drawerContent}
-            //     />
-            //   </ListItem>
-            // </Link>
-            <a id={`#${text.toLowerCase()}`} key={text}>
-              <ListItem button>
-                <ListItemIcon className={classes.drawerContent}>
-                  {icon}
-                </ListItemIcon>
-                <ListItemText
-                  primary={text}
-                  className={classes.drawerContent}
-                />
-              </ListItem>
-            </a>
-          );
-        })}
-      </List>
-    </Drawer>
-  );
+	return (
+		<Drawer
+			anchor="right"
+			open={open}
+			onClose={() => setOpen(false)}
+			classes={{ paper: classes.drawerPaper }}
+		>
+			<Toolbar />
+			<Divider className={classes.divider} />
+			<List>
+				{list.map((item) => {
+					const text = Object.keys(item)[0];
+					const icon = item[text];
+					return (
+						<Link href={`/#${text.toLowerCase()}`} key={text}>
+							<ListItem button>
+								<ListItemIcon className={classes.drawerContent}>
+									{icon}
+								</ListItemIcon>
+								<ListItemText
+									primary={text}
+									className={classes.drawerContent}
+								/>
+							</ListItem>
+						</Link>
+					);
+				})}
+			</List>
+		</Drawer>
+	);
 };
 
 export default MyDrawer;
