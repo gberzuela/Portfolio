1c3a2d8dc506a3211ed65e44286ecf7b7763aa49
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-03-16 13:08:58 -0700

    UPDATE: staying dry with section titles

--- client/components/CustomTimeline.js
+++ client/components/CustomTimeline.js
@@ -1,57 +1,36 @@
-import React from "react";
+import React from 'react';
 
-import {
-  Container,
-  Divider,
-  makeStyles,
-  Typography,
-  useMediaQuery,
-} from "@material-ui/core";
-import { Timeline } from "@material-ui/lab";
+import { Container, makeStyles, useMediaQuery } from '@material-ui/core';
+import { Timeline } from '@material-ui/lab';
 
-import Fade from "react-reveal/Fade";
+import Fade from 'react-reveal/Fade';
 
-import data from "../timelineData";
-import CustomTimelineItem from "./CustomTimelineItem";
+import data from '../data/timeline';
+import Title from './Title';
+import CustomTimelineItem from './CustomTimelineItem';
 
-const styles = makeStyles((theme) => {
-  return {
-    container: {
-      padding: "0 16%",
-    },
-    divider: {
-      width: "33%",
-      marginLeft: "10px",
-    },
-    titleContainer: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      padding: 0,
-      marginBottom: "10px",
-    },
-  };
+const styles = makeStyles({
+	container: {
+		padding: '100px 16%',
+	},
 });
 
 const CustomTimeline = () => {
-  const classes = styles();
-  const mediaQuery = useMediaQuery("(max-width: 767px)");
+	const classes = styles();
+	const mediaQuery = useMediaQuery('(max-width: 767px)');
 
-  return (
-    <Container className={classes.container} id="background">
-      <Fade up>
-        <Container className={classes.titleContainer}>
-          <Typography variant="h4">Background</Typography>
-          <Divider className={classes.divider} />
-        </Container>
-        <Timeline align={mediaQuery ? "left" : "alternate"}>
-          {data.map((entry) => (
-            <CustomTimelineItem key={entry.key} data={entry} />
-          ))}
-        </Timeline>
-      </Fade>
-    </Container>
-  );
+	return (
+		<Container className={classes.container} id="background">
+			<Fade up>
+				<Title title="Background" />
+				<Timeline align={mediaQuery ? 'left' : 'alternate'}>
+					{data.map((entry) => (
+						<CustomTimelineItem key={entry.key} data={entry} />
+					))}
+				</Timeline>
+			</Fade>
+		</Container>
+	);
 };
 
 export default CustomTimeline;
---
+++ client/components/Title.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Container, Divider, makeStyles, Typography } from '@material-ui/core';
+
+const styles = makeStyles({
+	container: {
+		display: 'flex',
+		alignItems: 'center',
+		padding: 0,
+		marginBottom: '10px',
+	},
+	divider: {
+		width: '33%',
+		marginLeft: '10px',
+	},
+});
+
+const Title = ({ title }) => {
+	const classes = styles();
+	return (
+		<Container className={classes.container}>
+			<Typography variant="h4">{title}</Typography>
+			<Divider className={classes.divider} />
+		</Container>
+	);
+};
+
+export default Title;
