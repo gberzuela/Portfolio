b83d6b4a7990d32975986a19fdbf3a3dd372a529
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-04-12 20:55:22 -0700

    STYLE: title bold

--- client/index.js
+++ client/index.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 
-import { NavBar, HomePage } from './components';
+import { NavBar, HomePage } from "./components";
 
 const theme = createMuiTheme({
-	typography: {
-		allVariants: {
-			color: 'white',
-		},
-	},
-	palette: {
-		divider: '#5c5c5c',
-		paper: '#424242',
-		blue: '#0086d4',
-		secondary: {
-			main: '#fff',
-		},
-	},
+  typography: {
+    allVariants: {
+      color: "white",
+    },
+    h3: {
+      fontWeight: "bold",
+    },
+  },
+  palette: {
+    divider: "#5c5c5c",
+    paper: "#424242",
+    blue: "#0086d4",
+    secondary: {
+      main: "#fff",
+    },
+  },
 });
 
 const App = () => {
-	return (
-		<ThemeProvider theme={theme}>
-			<NavBar />
-			<HomePage />
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <NavBar />
+      <HomePage />
+    </ThemeProvider>
+  );
 };
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById("app"));
