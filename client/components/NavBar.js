0fb5f9f95f9027a09fec0eee588a74765846233e
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-04-12 20:52:31 -0700

    UPDATE: hamburger menu cursor style on hover

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,32 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { AppBar, makeStyles, Toolbar } from '@material-ui/core';
-import { withStyles } from '@material-ui/styles';
-import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, makeStyles, Toolbar } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
+import MenuIcon from "@material-ui/icons/Menu";
 
-import Drawer from './Drawer';
+import Drawer from "./Drawer";
 
 const styles = makeStyles((theme) => ({
-	appBar: {
-		backgroundColor: 'black',
-	},
-	toolbar: {
-		justifyContent: 'flex-end',
-	},
+  appBar: {
+    backgroundColor: "black",
+  },
+  toolbar: {
+    justifyContent: "flex-end",
+  },
+  hamburger: {
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
 }));
 
 const NavBar = () => {
-	const classes = styles();
-	const [open, setOpen] = useState(false);
+  const classes = styles();
+  const [open, setOpen] = useState(false);
 
-	return (
-		<AppBar position="fixed" className={classes.appBar}>
-			<Toolbar className={classes.toolbar}>
-				<MenuIcon fontSize="large" onClick={() => setOpen(true)} />
-				{open && <Drawer open={open} setOpen={setOpen} />}
-			</Toolbar>
-		</AppBar>
-	);
+  return (
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar className={classes.toolbar}>
+        <MenuIcon
+          fontSize="large"
+          onClick={() => setOpen(true)}
+          className={classes.hamburger}
+        />
+        {open && <Drawer open={open} setOpen={setOpen} />}
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default withStyles(styles)(NavBar);
